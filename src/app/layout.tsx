a5fa8e8b94afffafc2a712ad8f5f1a3371d2900f
Jean Alexsander Caesar <jeanalexander183@gmail.com>
2026-08-29 13:37:23 +0700

    edit title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
 
 export const metadata: Metadata = {
-  title: "CV ANEKA TECHNIC PURWOKERTO",
+  title: "CV Aneka Technic",
   description: "Layanan perbaikan dan cuci AC bergaransi.",
 };
 
